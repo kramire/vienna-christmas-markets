1f1f3253b50935a1e7b12125cf0f73db6ca20a06
Katie Ramire <katieramire@Katies-MacBook-Pro.local>
2023-06-01 22:10:21 +0200

    move result item styles to tailwind

--- src/containers/resultList/resultItem.tsx
+++ src/containers/resultList/resultItem.tsx
@@ -7,7 +7,6 @@ import FilledHeartIcon from '../../assets/filledHeart.svg';
 import EmptyHeartIcon from '../../assets/emptyHeart.svg';
 import { localizeDate } from '../../utils/localizeDate';
 import { Event, Market, ResultType } from '../../app.types';
-import Flex from '../../components/flex';
 import { theme } from '../../theme';
 import { GOOGLE_MAPS_LINK, weekDays } from './resultList.constants';
 
@@ -25,13 +24,15 @@ const ResultItem = ({ result, isFavorite, toggleFavoriteResult }: Props) => {
   }, []);
 
   return (
-    <li key={result.id} className="result-item">
-      <Flex justifyContent="space-between" gap="12px">
+    <li
+      key={result.id}
+      class="flex flex-col justify-between w-full px-4 py-6 gap-3"
+      style={{ boxShadow: '0 16px 64px -16px rgba(46,55,77,.24)' }}
+    >
+      <div class="flex justify-between gap-3">
         <h3
+          class="text-xl font-semibold"
           style={{
-            fontWeight: 'semi-bold',
-            fontSize: '22px',
-            lineHeight: '32px',
             color: theme.colors.darkGreen,
           }}
         >
@@ -53,101 +54,83 @@ const ResultItem = ({ result, isFavorite, toggleFavoriteResult }: Props) => {
             outline: 'none',
           }}
         />
-      </Flex>
-      <Flex flexDirection="column" gap="12px">
-        <Flex alignItems="center" gap="16px">
+      </div>
+      <div class="flex align-center gap-4">
+        <img
+          src={LocationIcon}
+          loading="lazy"
+          alt="district location"
+          width={16}
+          height={16}
+        />
+        <a
+          href={`${GOOGLE_MAPS_LINK}&query=${result.coordinates.lat},${result.coordinates.lng}`}
+          target="_blank"
+          aria-label="Google maps link"
+        >
+          <p style={{ textDecoration: 'underline', cursor: 'pointer' }}>
+            {result.district}
+          </p>
+        </a>
+      </div>
+      {result.start && result.end && (
+        <div class="flex align-center gap-4">
           <img
-            src={LocationIcon}
+            src={CalendarIcon}
             loading="lazy"
-            alt="district location"
+            alt="calendar"
             width={16}
             height={16}
           />
-          <a
-            href={`${GOOGLE_MAPS_LINK}&query=${result.coordinates.lat},${result.coordinates.lng}`}
-            target="_blank"
-            aria-label="Google maps link"
-          >
-            <p style={{ textDecoration: 'underline', cursor: 'pointer' }}>
-              {result.district}
-            </p>
-          </a>
-        </Flex>
-        {result.start && result.end && (
-          <Flex alignItems="center" gap="16px">
-            <img
-              src={CalendarIcon}
-              loading="lazy"
-              alt="calendar"
-              width={16}
-              height={16}
-            />
-            <p>
-              {localizeDate(result.start, language)} -{' '}
-              {localizeDate(result.end, language)}
-            </p>
-          </Flex>
-        )}
-        <Flex gap="16px">
-          <img
-            src={ClockIcon}
-            loading="lazy"
-            alt="clock"
-            width={16}
-            height={16}
-            style={{ marginTop: '7px' }}
-          />
-          <Flex
-            flexDirection="column"
-            gap="0px 24px"
-            style={{
-              flexWrap: 'wrap',
-              height: '96px',
-            }}
-          >
-            {result.times.map((time, timeIdx) => (
-              <Flex gap="12px" style={{ fontSize: '14px' }}>
-                <p style={{ width: '14px', textAlign: 'center' }}>
-                  {weekDays[timeIdx]}
+          <p>
+            {localizeDate(result.start, language)} -{' '}
+            {localizeDate(result.end, language)}
+          </p>
+        </div>
+      )}
+      <div class="flex gap-4">
+        <img
+          src={ClockIcon}
+          loading="lazy"
+          alt="clock"
+          width={16}
+          height={16}
+          style={{ marginTop: '7px' }}
+        />
+        <div class="h-24 flex flex-col flex-wrap gap-x-6">
+          {result.times.map((time, timeIdx) => (
+            <div class="flex gap-3" style={{ fontSize: '14px' }}>
+              <p style={{ width: '14px', textAlign: 'center' }}>
+                {weekDays[timeIdx]}
+              </p>
+              {
+                <p key={`${result.id}_${timeIdx}`} style={{ fontSize: '14px' }}>
+                  {Array.isArray(time) ? `${time[0]} - ${time[1]}` : 'Closed'}
                 </p>
-                {
-                  <p
-                    key={`${result.id}_${timeIdx}`}
-                    style={{ fontSize: '14px' }}
-                  >
-                    {Array.isArray(time) ? `${time[0]} - ${time[1]}` : 'Closed'}
-                  </p>
-                }
-              </Flex>
-            ))}
-          </Flex>
-        </Flex>
-        {result.website && (
-          <Flex
-            alignItems="center"
-            gap="16px"
-            style={{
-              margin: '8px 0px',
-            }}
-          >
-            <img
-              src={InfoIcon}
-              loading="lazy"
-              alt="info"
-              width={16}
-              height={16}
-            />
+              }
+            </div>
+          ))}
+        </div>
+      </div>
+      {result.website && (
+        <div class="flex align-center gap-4">
+          <img
+            src={InfoIcon}
+            loading="lazy"
+            alt="info"
+            width={16}
+            height={16}
+          />
 
-            <a
-              href={result.website}
-              target="_blank"
-              alt={`Homepage for the ${result.name} event.`}
-            >
-              Website
-            </a>
-          </Flex>
-        )}
-      </Flex>
+          <a
+            href={result.website}
+            target="_blank"
+            alt={`Homepage for the ${result.name} event.`}
+          >
+            Website
+          </a>
+        </div>
+      )}
     </li>
   );
 };
