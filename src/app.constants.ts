db12c0817a03e92416a8db316adf775ab88771d6
Katie Ramire <katieramire@Katies-MacBook-Pro.local>
2023-06-24 08:24:06 +0200

    add more photos. rename image file names

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -2,23 +2,27 @@ export const FOOTER_HEIGHT = 60
 
 export const FAVORITED_MARKETS_LOCAL_STORAGE_KEY = 'favoritedMarkets'
 
-import IMG_SRC_1 from './data/images/Musuem_Quartier_Christmas_market.webp'
-import IMG_SRC_3 from './data/images/Tuerkenschanzpark_Christmas_market.webp'
-import IMG_SRC_5 from './data/images/Genuss_Christmas_market.webp'
-import IMG_SRC_6 from './data/images/Altes_AKH_Christmas_market.webp'
-import IMG_SRC_8 from './data/images/Maria_ Theresien_Platz_Christmas_market.webp'
-import IMG_SRC_12 from './data/images/Liechtenstein_Christmas_market.webp'
-import IMG_SRC_16 from './data/images/Schoenbrunn_Christmas_market.webp'
-import IMG_SRC_17 from './data/images/Rathaus_Christmas_market.webp'
-import IMG_SRC_18 from './data/images/Prater_Christmas_market.webp'
+import IMG_SRC_1 from './data/images/Musuem-Quartier-Christmas-market.webp'
+import IMG_SRC_3 from './data/images/Tuerkenschanzpark-Christmas-market.webp'
+import IMG_SRC_4 from './data/images/Saint-Stephens-Cathedral-at-Christmas.webp'
+import IMG_SRC_5 from './data/images/Genuss-Christmas-market.webp'
+import IMG_SRC_6 from './data/images/Altes-AKH-Christmas-market.webp'
+import IMG_SRC_8 from './data/images/Maria-Theresien-Platz-Christmas-market.webp'
+import IMG_SRC_12 from './data/images/Liechtenstein-Christmas-market.webp'
+import IMG_SRC_14 from './data/images/Karlsplatz-Christmas-market.webp'
+import IMG_SRC_16 from './data/images/Schoenbrunn-Christmas-market.webp'
+import IMG_SRC_17 from './data/images/Rathaus-Christmas-market.webp'
+import IMG_SRC_18 from './data/images/Prater-Christmas-market.webp'
 
 export const resultToImgUrlMapping: { [key in number]: string } = {
   1: IMG_SRC_1,
   3: IMG_SRC_3,
+  4: IMG_SRC_4,
   5: IMG_SRC_5,
   6: IMG_SRC_6,
   8: IMG_SRC_8,
   12: IMG_SRC_12,
+  14: IMG_SRC_14,
   16: IMG_SRC_16,
   17: IMG_SRC_17,
   18: IMG_SRC_18,
