fa2d400ee7672c9ed829970653925f4d7a77bd8e
Katie Ramire <katieramire@Katies-MacBook-Pro.local>
2023-10-14 13:17:40 +0200

    group markets on visits page by district

---
+++ src/app/visits/helpers/group-by-district.ts
@@ -0,0 +1,17 @@
+import { Market } from '../../../app.types'
+
+export function groupByDistrict(markets: Array<Market>) {
+  const districts: { [key in string]: Array<Market> } = {}
+
+  markets.forEach((market) => {
+    const currentMarkets = districts[market.district]
+
+    if (currentMarkets) {
+      districts[market.district] = [...currentMarkets, market]
+    } else {
+      districts[market.district] = [market]
+    }
+  })
+
+  return districts
+}
--- src/app/visits/page.tsx
+++ src/app/visits/page.tsx
@@ -2,6 +2,7 @@ import { Market, Event, ResultType } from '../../app.types'
 import data from '../../data.json'
 import { sortRedultsByDate } from '../../utils/sort-results-by-date'
 import VisitedToggle from './components/visitedToggle'
+import { groupByDistrict } from './helpers/group-by-district'
 
 export default function VisitsPage() {
   const results = data as Array<Market | Event>
@@ -9,21 +10,24 @@ export default function VisitsPage() {
     .filter((result) => result.type === ResultType.MARKET && result.isActive)
     .sort(sortRedultsByDate) as Array<Market>
 
+  const marketsByDistrict = groupByDistrict(markets)
+  const districts = Object.keys(marketsByDistrict).sort()
+
   return (
-    <div className="flex flex-col justify-center items-center gap-3 pt-3">
-      <h2 className="text-lg align-center font-bold">Which markets have you visited?</h2>
-      <div className="m-6 text-base">
-        <div className="flex flex-col gap-3">
-          {markets.map((market) => (
-            <div key={market.id} className="flex gap-4">
-              <div className="flex-1">
-                {market.name}
-                <p className="text-sm">{market.district}</p>
+    <div className="flex flex-col justify-center items-center gap-8 p-6">
+      <h2 className="text-lg align-center font-bold text-green-950">Which markets have you visited?</h2>
+      <div className="flex flex-col gap-4">
+        {districts.map((district) => (
+          <div key={`district_${district}`} className="shadow-md p-4">
+            <h3 className="font-bold text-lg text-green-950 mb-4">{district}</h3>
+            {marketsByDistrict[district].map((market) => (
+              <div key={market.id} className="flex gap-4 text-base mb-4">
+                <div className="flex-1">{market.name}</div>
+                <VisitedToggle marketId={market.id} />
               </div>
-              <VisitedToggle marketId={market.id} />
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   )
