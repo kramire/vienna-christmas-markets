5eb87efcaa37e0e808789fe12c2a0f37d3902cc5
Katie Ramire <katieramire@Katies-MacBook-Pro.local>
2024-11-16 22:43:04 +0100

    update lights data

--- src/app/lights/Lights.constants.ts
+++ src/app/lights/Lights.constants.ts
@@ -3,6 +3,8 @@ import IMG_SRC_5 from '../../../public/graben.webp'
 import IMG_SRC_7 from '../../../public/kaertner-strasse.webp'
 import IMG_SRC_8 from '../../../public/kohlmarkt.webp'
 import IMG_SRC_9 from '../../../public/neuer-markt.webp'
+import IMG_SRC_11 from '../../../public/rotenturmstrasse.webp'
+import IMG_SRC_17 from '../../../public/neubaugasse.webp'
 import IMG_SRC_19 from '../../../public/servitenviertel.webp'
 import { StaticImageData } from 'next/image'
 
@@ -12,5 +14,7 @@ export const resultToImgUrlMapping: { [key in number]: StaticImageData } = {
   7: IMG_SRC_7,
   8: IMG_SRC_8,
   9: IMG_SRC_9,
+  11: IMG_SRC_11,
+  17: IMG_SRC_17,
   19: IMG_SRC_19,
 }
