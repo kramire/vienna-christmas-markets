4f1bf14d8432eb7e1fb21b837dad5d94f0277519
Katie Ramire <katieramire@Katies-MacBook-Pro.local>
2025-10-20 18:28:26 +0200

    update data - and css for result cards

--- src/components/ResultList/ResultCard/ResultCard.tsx
+++ src/components/ResultList/ResultCard/ResultCard.tsx
@@ -35,15 +35,15 @@ const ResultCard = ({ result, isFavorite, toggleFavoriteResult }: Props) => {
   }, [])
 
   return (
-    <li key={id} className="relative flex">
-      <Link href={`/${result.slug}`} className="flex w-full flex-col shadow-md">
-        <div className="relative h-64 w-full overflow-hidden rounded-t-lg">
+    <li key={id} className="relative flex overflow-hidden rounded-lg shadow-md">
+      <Link href={`/${result.slug}`} className="flex w-full flex-col">
+        <div className="relative h-64 w-full">
           <FavoriteButton isFavorite={isFavorite} onClick={toggleFavoriteResult(id)} />
           <CardImage imgSrc={imgSrc} altText={name} />
         </div>
         <div className="flex w-full flex-1 flex-col space-y-4 p-4">
           <h2 className="text-lg font-semibold text-green-950 sm:text-xl">{name}</h2>
-          <dl className="space-y-3 text-sm sm:text-base sm:[&_dd]:text-sm sm:[&_dd]:font-semibold sm:[&_dt]:font-normal sm:[&_img]:row-span-2 sm:[&_img]:mt-1">
+          <dl className="space-y-3 text-base sm:[&_dd]:font-semibold sm:[&_dt]:font-normal sm:[&_img]:row-span-2 sm:[&_img]:mt-1">
             <div className="flex grid-cols-[12px_1fr] items-center gap-2 sm:grid sm:items-start sm:gap-x-3 sm:gap-y-1">
               <Image src={LocationIcon} width={12} height={12} alt="" />
               <dd>District</dd>
