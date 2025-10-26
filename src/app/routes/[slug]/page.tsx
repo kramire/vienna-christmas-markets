9f8888a3ef52d305932721b7dab21c1c2dcf2e86
Katie Ramire <katieramire@Katies-MacBook-Pro.local>
2025-10-26 20:14:52 +0100

    add line and tooltip to route map - update map marker

--- src/app/routes/[slug]/page.tsx
+++ src/app/routes/[slug]/page.tsx
@@ -19,14 +19,21 @@ export default function WalkingRoutesPage({ params }: { params: { slug: string }
     Market | Event | StreetLights
   >
 
+  const routePath = stops
+    .map((stop) => resultsForMap.find((result) => result.id === stop.id)?.coordinates)
+    .filter((coord) => coord !== undefined) as Array<{ lat: number; lng: number }>
+
   const firstStop = stops.find((stop) => stop.order === 1)
   const firstStopCoordinates = resultsForMap.find((result) => result.id === firstStop?.id)?.coordinates
 
   return (
     <div className="m-auto flex w-full flex-col gap-4 p-4 md:h-[calc(100vh-52px)] lg:gap-6 lg:py-6">
       <div className="flex flex-col gap-1 md:gap-3">
-        <h1 className="text-xl font-bold text-gray-800 sm:text-3xl md:text-4xl">{`Route ${id}: ${name}`}</h1>
+        <h1 className="text-xl font-bold text-green-950 sm:text-3xl md:text-4xl">{`Route ${id}: ${name}`}</h1>
         <p className="text-sm md:text-base">{description}</p>
+        <a href="#map" className="text-right text-sm font-medium text-green-950 underline md:hidden">
+          Scroll to map
+        </a>
       </div>
       <div className="flex w-full flex-col gap-3 md:h-[calc(100%-80px)] md:flex-row md:[&>div]:flex-1">
         <ul className="flex h-full w-full flex-col gap-4 md:-mx-1 md:w-[60%] md:overflow-y-auto md:px-1">
@@ -37,8 +44,8 @@ export default function WalkingRoutesPage({ params }: { params: { slug: string }
               if (type === ResultType.MARKET || type === ResultType.EVENT) {
                 const result = resultsForMap.find((res) => res.id === id) as Market | Event
                 return (
-                  <Link href={`/${result.slug}`}>
-                    <RouteStopCard key={id} stopId={id} stopDescription={description} idx={idx} />
+                  <Link key={id} href={`/${result.slug}`}>
+                    <RouteStopCard stopId={id} stopDescription={description} idx={idx} />
                   </Link>
                 )
               }
@@ -51,6 +58,7 @@ export default function WalkingRoutesPage({ params }: { params: { slug: string }
           zoom={mapZoom}
           center={firstStopCoordinates}
           popUpVariant="text"
+          path={routePath}
         />
       </div>
     </div>
