3a1e4efce7b20a8765120a7a2b6fa588a7a8968c
Katie Ramire <katieramire@Katies-MacBook-Pro.local>
2025-11-11 19:01:09 +0100

    rework the icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Vienna Christmas Markets',
-    short_name: 'ViennaChristmas',
+    short_name: 'Vienna 🎄',
     description: "A list of this year's Christmas festivities in Vienna, Austria.",
     start_url: '/',
     display: 'standalone',
@@ -11,9 +11,16 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#092e0b',
     icons: [
       {
-        src: '/mistletoe.png',
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   }
